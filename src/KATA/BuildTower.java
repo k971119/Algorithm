dca4c4eec71985ee47b4b2ceef5f7646674d91ec
k971119 <hanyul97@naver.com>
2023-03-16 15:22:39 +0900

    Update BuildTower.java

--- src/KATA/BuildTower.java
+++ src/KATA/BuildTower.java
@@ -4,19 +4,39 @@ public class BuildTower {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		towerBuilder(3);
+		for(String floor : towerBuilder(5)) {
+			System.out.println(floor);
+		}
 	}
 	
-	public static void towerBuilder(int nFloors) {
+	public static String[] towerBuilder(int nFloors) {
+		/*
 		int lSide = nFloors;
 		int rSide = nFloors;
+		
+		String[] result = new String[nFloors];
+		
 		for(int h = 0; h < nFloors; h++) {		//Height
+			String Floor = "";
 			for(int w = 0; w < nFloors+h; w++) {
-				System.out.print((lSide <= w+1) && (rSide >= w+1)? "*":" ");
+				Floor += ((lSide <= w+1) && (rSide >= w+1)? "*":" ");
 			}
+			if((nFloors*2)-1 > Floor.length()) {
+				for(int i = Floor.length(); i < (nFloors*2)-1; i++) {
+					Floor+=" ";
+				}
+			}
+			result[h] = Floor;
 			lSide--;
 			rSide++;
-			System.out.println();
 		}
+		return result;
+		*/
+		String t[] = new String[nFloors], e;
+	    
+	    for (int i = 0; i < nFloors; i++)
+	      t[i] = (e = " ".repeat(nFloors-i-1)) + "*".repeat(i+i+1) + e;
+	    
+	    return t;
 	}
 }
