3cfab3562894cbcae8a16c8ab975e88daf7178e0
k971119 <hanyul97@naver.com>
2023-03-20 14:55:55 +0900

    Create MexicanWave.java

---
+++ src/KATA/MexicanWave.java
@@ -0,0 +1,19 @@
+package KATA;
+
+public class MexicanWave {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static String[] wave(String str) {
+		String[] letters = str.split("");
+		String[] words = new String[letters.length];
+		for(int i = 0; i < letters.length; i++) {
+			letters[i].toUpperCase();
+		}
+		return new String[] {};
+	}
+
+}
